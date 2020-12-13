382cbc4f377cd2110b91e88d276f0d2769eaef7b
courserav <lvsmlit@gmail.com>
2020-12-12 19:13:41 -0500

    commit tree 566354b2b338c2391e7b3709af01f710baef8d8f

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,22 @@
 def square_array(array)
   # your code here
+
+  index_counter = 0
+
+  array.each do |number|
+    array[index_counter] = number * number
+    index_counter += 1
+  end
 end
+
+def square_advanced(array)
+  array.collect do |number| number * number end
+end 
+
+first_numbers = [1,2,3]
+square_array(first_numbers)
+puts first_numbers
+
+second_numbers = [4,5,6]
+second_numbers = square_advanced(second_numbers)
+puts second_numbers
